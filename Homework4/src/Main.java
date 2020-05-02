e4aabda9982e7e8d380649d4c0bed8a74197ead8
Helena Jäe <56818449+helenajae@users.noreply.github.com>
2020-05-02 12:55:15 +0300

    Update Main.java

--- Homework4/src/Main.java
+++ Homework4/src/Main.java
@@ -1,30 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
+        //Get
         Aine oop = new Aine("RT – 016", "Objektorienteeritud programmeerimine", 5);
         System.out.println(oop.getKood());
         System.out.println(oop.getNimetus());
         System.out.println(oop.getAinepunktideArv());
 
-        System.out.println(String.format("Õppeaine Java OOP läbimiseks kulub %d tundi.", oop.labimine()));
+        System.out.println(String.format("Õppeaine Java objektorienteeritud programmeermine läbimiseks kulub %d tundi.", oop.labimine()));
 
+        System.out.println();
+
+        //Set
+        //Suhtlemispsühholoogia
         oop.setKood("RT-004");
         oop.setNimetus("Suhtlemispsühholoogia");
         oop.setAinepunktideArv(4);
-
-        System.out.println(String.format("Aine on muudetud: " + oop));
+        System.out.println(String.format("Õppeaine on muudetud: " + oop));
         System.out.println(String.format("Selle aine läbimiseks kulub: %d", oop.labimine()));
 
 
-        //Testige konstruktori ja meetodite kasutamist test-klassis
-
+        //Konstruktori ja meetodite kasutamine test-klassis
+        //Koostöömudelid
         Aine Koostoomudelid = new Aine("RT-003", "Koostöömudelid", 2);
         Aine[] RTained = {Koostoomudelid, oop};
         Oppekava robootikaTarkvara = new Oppekava("RT - VIL", RTained);
         System.out.println(robootikaTarkvara.toString());
 
+
     }
-
-
-
 } // end of Main
